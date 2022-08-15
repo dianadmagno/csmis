e15dd6c0261bb709640247d17a1c5775644d9219
Diana Magno <dianamagno20@gmail.com>
2022-08-15 16:34:18 +0800

    Added ethnic group seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,5 +19,6 @@ class DatabaseSeeder extends Seeder
         $this->call([EnlistmentTypeTableSeeder::class]);
         $this->call([RankTableSeeder::class]);
         $this->call([ReligionTableSeeder::class]);
+        $this->call([EthnicGroupTableSeeder::class]);
     }
 }
