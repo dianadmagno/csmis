424f88b6d9c360466174f9245f4268b726a56bf0
Diana Magno <dianamagno20@gmail.com>
2022-08-17 13:09:32 +0800

    Added personnel Type Reference

---
+++ app/Http/Controllers/References/PersonnelTypeController.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace App\Http\Controllers\References;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Models\References\PersonnelType;
+
+class PersonnelTypeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Request $request)
+    {
+        $keyword = $request->keyword;
+        return view('references.personnelType.index', [
+            'personnelTypes' => PersonnelType::where('name', 'LIKE', '%'.$keyword.'%')
+                        ->orWhere('description', 'LIKE', '%'.$keyword.'%')
+                        ->paginate(10)
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('references.personnelType.index');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        PersonnelType::create($request->all());
+        return redirect()->route('personnelType.index')->with('status', 'Personnel Type Created Successfully');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\References\PersonnelType  $personnelType
+     * @return \Illuminate\Http\Response
+     */
+    public function show(PersonnelType $personnelType)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\References\PersonnelType  $personnelType
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        return view('references.personnelType.edit', [
+            'personnelType' => PersonnelType::find($id)
+        ]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\References\PersonnelType  $personnelType
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $data = PersonnelType::find($id);
+        $data->update($request->all());
+
+        $personnelTypes = PersonnelType::paginate(10);
+        return view('references.personnelType.index', [
+            'personnelTypes' => $personnelTypes
+        ])->withStatus(__('Personnel Type successfully updated.'));
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\References\PersonnelType  $personnelType
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $id = PersonnelType::find($id);
+        $id->delete();
+        return redirect()->route('personnelType.index')->with('status', 'Personnel Type Archived Successfully');
+    }
+}
---
+++ resources/views/references/personnelType/index.blade.php
@@ -0,0 +1,102 @@
+@extends('layouts.app')
+
+@section('content')
+    @include('users.partials.header', [
+      'title' => __('List of Personnel Types')
+    ])   
+
+    <div class="container-fluid mt--7">
+          <!-- Page content -->
+          <div class="container-fluid mt--6">
+            <div class="row">
+              <div class="col">
+                <div class="card">
+                  <!-- Card header -->
+                  <form action="{{ route('personnelType.index') }}">
+                    <div class="card-header border-0">
+                      <div class="row align-items-center">
+                        <div class="col-3">
+                          <div class="form-group mb-0">
+                            <div class="input-group">
+                              <div class="input-group-prepend">
+                                <span class="input-group-text"><i class="ni ni-zoom-split-in"></i></span>
+                              </div>
+                              <input name="keyword" class="form-control" placeholder="Search" type="text">
+                            </div>
+                          </div>
+                        </div>
+                        <div class="col-2">
+                          <button type="submit" class="btn btn-default">Search</button>
+                        </div>
+                        <div class="col text-right">
+                            <a href="{{ route('personnelType.create') }}" class="btn btn-primary">Add Personnel Type</a>
+                        </div>
+                        @if (session('status'))
+                            <div class="col mt-1 alert alert-success alert-dismissible fade show" role="alert">
+                                {{ session('status') }}
+                                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                                    <span aria-hidden="true">&times;</span>
+                                </button>
+                            </div>
+                        @endif
+                      </div>
+                    </div>
+                  </form>
+                  <!-- Light table -->
+                  <div class="table-responsive">
+                    <table class="table align-items-center table-flush">
+                      <thead class="thead-light">
+                        <tr>
+                          <th scope="col">Name</th>
+                          <th scope="col">Desription</th>
+                          <th scope="col">Action</th>
+                        </tr>
+                      </thead>
+                      <tbody class="list">
+                        @if (count($personnelTypes) > 0)
+                          @foreach($personnelTypes as $personnelType)
+                            <tr>
+                              <th scope="row">
+                                <div class="media align-items-center">
+                                  <div class="media-body">
+                                    <span class="name mb-0 text-sm">{{ $personnelType->name }}</span>
+                                  </div>
+                                </div>
+                              </th>
+                              <td class="budget">
+                                {{ $personnelType->description }}
+                              </td>
+                              <td>
+                                <div class="row">
+                                  <form action="{{ route('personnelType.destroy', $personnelType->id) }}" method="post">
+                                    @csrf
+                                    @method('delete')
+                                    <a href="{{ route('personnelType.edit', $personnelType->id) }}" class="btn btn-success" type="button">Edit</a>
+                                    <button type="submit" onclick="return alert('Do you really want to archive this role?')" class="btn btn-danger">Archive</button>
+                                </form>
+                                </div>
+                              </td>
+                            </tr>
+                          @endforeach
+                        @else
+                            <tr class="text-center">
+                                <td colspan="5">No Available Data</td>
+                            </tr>
+                        @endif
+                      </tbody>
+                    </table>
+                  </div>
+                  <!-- Card footer -->
+                  @if (count($personnelTypes) > 0)
+                    <div class="card-footer">
+                      {{ $personnelTypes->links() }}
+                    </div>
+                  @endif
+                </div>
+              </div>
+            </div>
+          </div>
+
+        @include('layouts.footers.auth')
+    </div>
+@endsection
